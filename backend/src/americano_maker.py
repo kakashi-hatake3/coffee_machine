12ece3e1dd80264c0888f366cfed6dfee6061b53
kakashi-hatake3 <mayorov.art7@gmail.com>
2025-02-22 19:40:22 +0300

    nwm3

--- backend/src/americano_maker.py
+++ backend/src/americano_maker.py
@@ -7,12 +7,3 @@ class AmericanoMaker(CoffeeMaker):
         super().__init__(coffee_beans, water, milk)
         self.coffee_type = CoffeeTypes.americano
         self.coffee_cost = self.coffee_cost_in_portions[self.coffee_type]
-
-
-
-
-
-
-
-
-
